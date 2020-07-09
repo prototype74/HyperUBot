e8f3d1a01120aefb390af7901817d291e2eb8b73
Nuno Penim <nuno.penim@gmail.com>
2020-07-09 10:55:52 +0100

    main: basic main implementation

---
+++ tg_userbot/__main__.py
@@ -0,0 +1,14 @@
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+
+from tg_userbot import bot
+
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print("Invalid PhoneNumber")
+    exit(1)
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
