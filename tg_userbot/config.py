c3ff12a76532d85123ad9e441b07a8381732dd2a
Nuno Penim <nuno.penim@gmail.com>
2020-07-09 15:39:28 +0100

    general: added a small config.py

---
+++ tg_userbot/config.py
@@ -0,0 +1,7 @@
+class ConfigClass(object):
+    API_KEY = None
+    API_HASH = None
+
+# TO-DO:
+# 1 - Make this sample_config.py
+# 2 - Add a check to make sure no one just grabs and copies this, like in tgbot and tguserbot
