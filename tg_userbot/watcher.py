776c16e62dbc13b7353685207eac23e2018184fd
Nuno Penim <nuno.penim@gmail.com>
2020-07-09 16:36:11 +0100

    watcher: adjustments

--- tg_userbot/watcher.py
+++ tg_userbot/watcher.py
@@ -4,10 +4,12 @@ from tg_userbot import bot
 
 def watcher(**args): #bot command processor, maybe later for edited messages
     command = args.get('command', None)
+    #disable_edited = args.get('disable_edited', False)
     if command is not None and not command.startswith('(?i)'):
         args['pattern'] = '(?i)' + command
 
     def starter(func):
+        bot.add_event_handler(func, events.MessageEdited(**args))
         bot.add_event_handler(func, events.NewMessage(**args))
         return func
 
