4b5beb9e7422158b2fb340dbff81572f6fe4fe17
Nuno Penim <nuno.penim@gmail.com>
2020-07-09 16:35:18 +0100

    test_module: tweaks

--- tg_userbot/modules/test_module.py
+++ tg_userbot/modules/test_module.py
@@ -2,4 +2,4 @@ from tg_userbot.watcher import watcher
 
 @watcher(outgoing=True, pattern=r"^\.test$")
 async def tester(msg):
-    await msg.edit("`This is a test - I am working`")
+    await msg.edit("This is a test - I am working")
