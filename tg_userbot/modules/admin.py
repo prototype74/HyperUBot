1e5e091b933c5a67dd910dba0480058d6fe526e4
Nuno Penim <nuno.penim@gmail.com>
2020-07-11 15:36:39 +0100

    admin: implemented promoted (missing messages!)

--- tg_userbot/modules/admin.py
+++ tg_userbot/modules/admin.py
@@ -5,8 +5,8 @@ from tg_userbot.watcher import watcher
 # Telethon Stuff
 from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditBannedRequest
-from telethon.errors.rpcerrorlist import UserIdInvalidError
-from telethon.tl.types import ChatAdminRights, ChatBannedRights, User
+from telethon.errors.rpcerrorlist import UserIdInvalidError, ChatAdminRequiredError, AdminsTooMuchError
+from telethon.tl.types import ChatAdminRights, ChatBannedRights, User, ChannelParticipantsAdmins
 
 # Misc
 from asyncio import sleep
@@ -18,10 +18,11 @@ KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
 MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
 UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 ADMIN_RIGHTS = ChatAdminRights(add_admins=False, invite_users=True, change_info=False, ban_users=True, delete_messages=True, pin_messages=True)
+DEMOTE_RIGHTS = ChatAdminRights(add_admins=None, invite_users=None, change_info=None, ban_users=None, delete_messages=None, pin_messages=None)
 
 # Done: Ban, Unban, Kick
-# Missing: Mute, Unmute, Promote, Demote
-# Maybe: remove deleted accounts, admin and user lists
+# Missing: Mute, Unmute, Demote, RM DL ACC, Pins
+# Maybe: admin list, user list
 
 @watcher(outgoing=True, pattern=r"^\.ban(?: |$)(.*)")
 async def ban(banning):
@@ -91,3 +92,61 @@ async def kick(kicker):
     await kicker.client(EditBannedRequest(kicker.chat_id, user.id, ChatBannedRights(until_date=None)))
     await kicker.edit(msgRep.KICKED_SUCCESSFULLY.format(str(user.id)))
     return
+
+@watcher(outgoing=True, pattern=r"^\.promote(?: |$)(.*)")
+async def promote(promt):
+    chat = await promt.get_chat()
+    if isinstance(chat, User):
+        await promt.edit("`Only-Chats-Groups`")
+        return
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not admin and not creator:
+        await promt.edit(msgRep.NOT_ADMIN)
+        return
+    get_user = await get_user_from_event(promt)
+    if isinstance(get_user, tuple):
+        user, rank = get_user
+    else:
+        user = get_user
+        rank = ""
+    if not rank:
+        rank = ""
+    if user:
+        pass
+    else:
+        return
+    if not isinstance(user, User):
+        await promt.edit("`NOT-A-USER`")
+        return
+    try:
+        async for member in promt.client.iter_participants(promt.chat_id, filter=ChannelParticipantsAdmins):
+            if user.id == member.id:
+                if user.is_self:
+                    await promt.edit("`PROMT-SELF`")
+                else:
+                    await promt.edit("`ADM-ALR`")
+                return
+    except ChatAdminRequiredError as err:
+        await promt.edit("`NEED-ADM`")
+        return
+    await promt.edit("`PROMTING`")
+    try:
+        if creator:
+            await promt.client(
+                EditAdminRequest(promt.chat_id, user.id, ADMIN_RIGHTS, rank))
+        else:
+            admin.add_admins = False
+            if all(getattr(admin, right) is False for right in vars(admin)):
+                return await promt.edit("`NO-ADD-ADMIN-RIGHT`")
+            await promt.client(
+                EditAdminRequest(promt.chat_id, user.id, admin, rank))
+        await promt.edit("`PRMT-SUCC`")
+    except AdminsTooMuchError:
+        await promt.edit("`TOO-MANY-ADM`")
+        return
+    except BadRequestError:
+        await promt.edit(msgRep.NO_PERMS)
+        return
+    return
+
