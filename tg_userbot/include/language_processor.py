3f51e8bb5c698a0c138da30e3dbd377d4f33e131
nunopenim <nuno.penim@gmail.com>
2020-08-14 15:01:41 +0100

    languaging: temporarly removed german because it's incomplete, fixed the pycharm import thingy

--- tg_userbot/include/language_processor.py
+++ tg_userbot/include/language_processor.py
@@ -2,7 +2,7 @@ from tg_userbot import LANG
 from importlib import import_module
 
 # Temp because pyCharm needs for autocompletion and I am being a Lazy S.O.B. again
-import tg_userbot.translations.en as text
+# import tg_userbot.translations.en as text
 
 # Language selector logic
 
