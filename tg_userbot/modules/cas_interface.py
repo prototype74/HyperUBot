91f498522b4957395d11b07279955cf16e5b5893
nunopenim <nuno.penim@gmail.com>
2020-08-26 14:59:13 +0100

    cas_interface: used our fetch user + corrections

--- tg_userbot/modules/cas_interface.py
+++ tg_userbot/modules/cas_interface.py
@@ -2,36 +2,10 @@
 import tg_userbot.include.cas_api as cas_api
 from tg_userbot.include.watcher import watcher
 from tg_userbot.include.language_processor import CasIntText as msgRep
+from tg_userbot.include.aux_funcs import fetch_user as get_user
 
 # Telethon stuff
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
 from telethon.errors import ChatAdminRequiredError
-
-async def get_user(event):  # kanged get user
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
-    else:
-        user = event.pattern_match.group(1)
-        if user.isnumeric():
-            user = int(user)
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(GetFullUserRequest(user_object.id))
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-    return replied_user
 
 @watcher(pattern=r"^\.cascheck(?: |$)(.*)", outgoing=True)
 async def caschecker(event):
@@ -68,7 +42,7 @@ async def caschecker(event):
             text += msgRep.TIME_ADDED
             text += str(parseArray[0])
             text += msgRep.UTC_INFO
-        await event.edit(text, parse_mode="html")
+    await event.edit(text, parse_mode="html")
     return
 
 @watcher(pattern=r"^\.groupcheck$", outgoing=True)
