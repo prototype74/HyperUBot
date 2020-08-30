00c89a66aa0601c84267e61b5fdb6625f25c583d
nunopenim <nuno.penim@gmail.com>
2020-08-30 09:51:52 +0100

    general: added Terminal module for first tests

---
+++ tg_userbot/modules/terminal.py
@@ -0,0 +1,18 @@
+# My stuff
+from tg_userbot import tgclient
+
+# Telethon stuff
+from telethon.events import NewMessage
+
+# Misc imports
+from subprocess import check_output
+
+@tgclient.on(NewMessage(pattern=r"^\.bash(?: |$)(.*)", outgoing=True))
+async def bash(command):
+    commandArray = command.text.split(" ")
+    bashCmd = ""
+    for word in commandArray: #building the command
+        if word is not ".bash":
+            bashCmd += word + " "
+    print(bashCmd)
+    return
