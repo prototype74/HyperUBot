2fff0f6f5ba7d0132fe7b0e5c0fd8393cd8a1e6f
prototype74 <46874884+prototype74@users.noreply.github.com>
2020-09-04 16:38:37 +0200

    aux_funcs: renamed local variable

--- tg_userbot/include/aux_funcs.py
+++ tg_userbot/include/aux_funcs.py
@@ -85,26 +85,26 @@ async def fetch_user(event=None, full_user=False, get_chat=False, org_author=Fal
 
 async def event_log(event, event_name: str, user_name=None, user_id=None, username=None,
                     chat_title=None, chat_link=None, chat_id=None, custom_text=None):
-    log = f"**{event_name}**\n"
+    text = f"**{event_name}**\n"
     if user_name and user_id and not username:
-        log += f"{msgsLang.LOG_USER}: [{user_name}](tg://user?id={user_id})\n"
+        text += f"{msgsLang.LOG_USER}: [{user_name}](tg://user?id={user_id})\n"
     elif user_name:
-        log += f"{msgsLang.LOG_USER}: {user_name}\n"
+        text += f"{msgsLang.LOG_USER}: {user_name}\n"
     if username:
-        log += f"{msgsLang.LOG_USERNAME}: @{username}\n"
+        text += f"{msgsLang.LOG_USERNAME}: @{username}\n"
     if user_id:
-        log += f"{msgsLang.LOG_USER_ID}: `{user_id}`\n"
+        text += f"{msgsLang.LOG_USER_ID}: `{user_id}`\n"
     if chat_title:
-        log += f"{msgsLang.LOG_CHAT_TITLE}: {chat_title}\n"
+        text += f"{msgsLang.LOG_CHAT_TITLE}: {chat_title}\n"
     if chat_link:
-        log += f"{msgsLang.LOG_CHAT_LINK}: @{chat_link}\n"
+        text += f"{msgsLang.LOG_CHAT_LINK}: @{chat_link}\n"
     if chat_id:
-        log += f"{msgsLang.LOG_CHAT_ID}: `{chat_id}`\n"
+        text += f"{msgsLang.LOG_CHAT_ID}: `{chat_id}`\n"
     if custom_text:
-        log += f"{custom_text}"
+        text += f"{custom_text}"
 
     try:
-        await event.client.send_message(LOGGING_CHATID, log)
+        await event.client.send_message(LOGGING_CHATID, text)
     except Exception as e:
         LOGS.warning(f"EVENT_LOG Exception {event_name}: {e}")
     return
