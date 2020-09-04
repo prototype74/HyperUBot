7458ce452928097a08da4dfa93dcdbff0523af77
nunopenim <nuno.penim@gmail.com>
2020-09-04 21:15:47 +0100

    misc: coinflip improvements

--- tg_userbot/modules/misc.py
+++ tg_userbot/modules/misc.py
@@ -10,12 +10,12 @@ import time
 
 @tgclient.on(NewMessage(pattern=r"^\.coinflip$", outgoing=True))
 async def coinflipper(coin):
-    number = random.randint(1, 10000)
     retStr = "COIN_LANDED_VAL"
     temp = ": "
     await coin.edit("THRWING_COIN")
     time.sleep(3) # gives more character if we wait a bit
-    if number % 2 == 0:
+    number = random.randint(1, 10000)
+    if (number % 2 == 0):
         retStr += temp + "HEADS"
     else:
         retStr += temp + "TAILS"
