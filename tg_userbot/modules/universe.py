3443dadb762c7e0dffa622371448ec93d56c00dc
nunopenim <nuno.penim@gmail.com>
2020-09-05 19:32:54 +0100

    universe: updated

--- tg_userbot/modules/universe.py
+++ tg_userbot/modules/universe.py
@@ -6,15 +6,17 @@ import tg_userbot.include.git_api as git
 from telethon.events import NewMessage
 
 UNIVERSE_URL = "nunopenim/module-universe"
+UNIVERSE_NAME = "modules-universe"
 
 # Maybe add just a single command, but multiple arguments?
 @tgclient.on(NewMessage(pattern=r"^\.universe$", outgoing=True))
 async def universe_checker(msg):
-    files = ""
+    files = "Files in **{}**".format(UNIVERSE_NAME)
     assets = git.getAssets(git.getReleaseData(git.getData(UNIVERSE_URL), 0))
     for asset in assets:
         assetName = git.getReleaseFileName(asset)
         assetURL = git.getReleaseFileURL(asset)
-        files += "[{}]({})".format(assetName, assetURL)
+        assetSize = git.getSize(asset)
+        files += "[{}]({}) - {} MB\n".format(assetName, assetURL, assetSize)
     await msg.edit(files, parse_mode='md')
     return
