167290af52fadf8b2b64c469ea11fb292733f745
prototype74 <46874884+prototype74@users.noreply.github.com>
2020-09-12 22:03:50 +0200

    main: updated Modules class
    Improved:
    - ALL_MODULES also includes not loaded modules now (before only sys and user list)
    - sum from ALL_MODULES length now

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -24,20 +24,21 @@ class _Modules:
     def __init__(self):
         self.__imported_module = None
         self.__load_modules_count = 0
-        self.__not_load_modules_count = 0
 
     def __load_modules(self) -> tuple:
+        all_modules = []
         sys_modules = []
         user_modules = []
         if OS and OS.startswith("win"):
-            sys_module_paths = glob(dirname(__file__) + "\\modules\\*.py")
-            user_module_paths = glob(dirname(__file__) + "\\modules_user\\*.py")
+            sys_module_paths = sorted(glob(dirname(__file__) + "\\modules\\*.py"))
+            user_module_paths = sorted(glob(dirname(__file__) + "\\modules_user\\*.py"))
         else:
-            sys_module_paths = glob(dirname(__file__) + "/modules/*.py")
-            user_module_paths = glob(dirname(__file__) + "/modules_user/*.py")
+            sys_module_paths = sorted(glob(dirname(__file__) + "/modules/*.py"))
+            user_module_paths = sorted(glob(dirname(__file__) + "/modules_user/*.py"))
         for module in sys_module_paths:
             if isfile(module) and module.endswith(".py"):
                 filename = basename(module)[:-3]
+                all_modules.append(filename)
                 try:
                     if not filename in NOT_LOAD_MODULES:
                         sys_modules.append(filename)
@@ -46,6 +47,7 @@ class _Modules:
         for module in user_module_paths:
             if isfile(module) and module.endswith(".py"):
                 filename = basename(module)[:-3]
+                all_modules.append(filename)
                 try:
                     if not filename in NOT_LOAD_MODULES:
                         if not filename in sys_modules:
@@ -57,11 +59,13 @@ class _Modules:
                         user_modules.append(filename)
                     else:
                         log.warning(f"Module '{filename}' not loaded as present in sys already")
-        return (sys_modules, user_modules)
+        return (all_modules, sys_modules, user_modules)
 
     def import_load_modules(self) -> bool:
-        sys_modules, user_modules = self.__load_modules()
+        all_modules, sys_modules, user_modules = self.__load_modules()
         try:
+            for module in sorted(all_modules):
+                ALL_MODULES.append(module)
             for module in sys_modules:
                 self.__imported_module = import_module("userbot.modules." + module)
                 LOAD_MODULES.append(module)
@@ -71,12 +75,6 @@ class _Modules:
                 LOAD_MODULES.append(module)
                 USER_MODULES.append(module)
                 self.__load_modules_count += 1
-            for module in sorted(sys_modules + user_modules):
-                ALL_MODULES.append(module)
-            if NOT_LOAD_MODULES:
-                for module in NOT_LOAD_MODULES:
-                    if module in ALL_MODULES:
-                        self.__not_load_modules_count += 1
             return True
         except ModuleNotFoundError as mnfe:
             log.error(f"Unable to load module: {mnfe}")
@@ -87,17 +85,13 @@ class _Modules:
     def loaded_modules(self) -> int:
         return self.__load_modules_count
 
-    def not_loaded_modules(self) -> int:
-        return self.__not_load_modules_count
-
 if __name__ == "__main__":
     try:
         modules = _Modules()
         if not modules.import_load_modules():
             quit(1)
         load_modules_count = modules.loaded_modules()
-        not_load_modules_count = modules.not_loaded_modules()
-        sum_modules = load_modules_count + not_load_modules_count
+        sum_modules = len(ALL_MODULES)
         with tgclient:
             tgclient.start()
             if not load_modules_count:
