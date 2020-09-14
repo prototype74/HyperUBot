5d7b1b8bf773b5b2cd5c8f6a1141d4f6ee227563
nunopenim <nuno.penim@gmail.com>
2020-09-14 18:28:09 +0200

    CAS_API: updated

--- userbot/include/cas_api.py
+++ userbot/include/cas_api.py
@@ -1,10 +1,9 @@
-import urllib.request as url
 import json
 import datetime
 import requests
 
 
-VERSION = "1.3.3"
+VERSION = "1.4.0"
 CAS_QUERY_URL = "https://api.cas.chat/check?user_id="
 DL_DIR = "./csvExports"
 
@@ -16,22 +15,20 @@ def get_user_data(user_id):
 def isbanned(userdata):
     return userdata['ok']
 
-def banchecker(user_id):
-    return isbanned(get_user_data(user_id))
+def banchecker(userdata):
+    return isbanned(userdata)
 
 def vercheck() -> str:
     return str(VERSION)
 
-def offenses(user_id):
-    userdata = get_user_data(user_id)
+def offenses(userdata):
     try:
         offenses = userdata['result']['offenses']
         return str(offenses) 
     except:
         return None
     
-def timeadded(user_id):
-    userdata = get_user_data(user_id)
+def timeadded(userdata):
     try:
         timeEp = userdata['result']['time_added']
         timeHuman = datetime.datetime.utcfromtimestamp(timeEp).strftime('%H:%M:%S, %d-%m-%Y')
