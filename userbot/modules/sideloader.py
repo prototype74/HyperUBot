f392289729745053c9f3dad261f7a3cbccfc4dda
nunopenim <nuno.penim@gmail.com>
2020-09-17 14:05:56 +0100

    sideloader: now autoreboots

--- userbot/modules/sideloader.py
+++ userbot/modules/sideloader.py
@@ -9,6 +9,16 @@ from userbot import tgclient, OS
 # Telethon stuff
 from telethon.events import NewMessage
 
+# Misc imports
+import sys
+import os
+import time
+
+if " " not in sys.executable:
+    EXECUTABLE = sys.executable
+else:
+    EXECUTABLE = '"' + sys.executable + '"'
+
 if OS and OS.startswith("win"):
     USER_MODULES_DIR = ".\\userbot\\modules_user\\"
 else:
@@ -20,11 +30,17 @@ async def sideload(event):
         msg = await event.get_reply_message()
         file = msg.file
         if not file.name.endswith(".py"):
-            await event.edit("IDIOT, this is not a .py file")
+            await event.edit("This is not a valid .py file! Cannot sideload this.")
             return
-        await tgclient.download_media(message=msg, file=USER_MODULES_DIR + file.name)
-        await event.edit("I think the download was finished?")
+        await event.edit("`Downloading...`")
+        await event.client.download_media(message=msg, file=USER_MODULES_DIR + file.name)
+        await event.edit("Successfully installed `{}`! Rebooting...".format(file.name))
+        time.sleep(1)
+        args = [EXECUTABLE, "-m", "userbot"]
+        await event.edit("Reboot complete!")
+        os.execle(sys.executable, *args, os.environ)
+        await event.client.disconnect()
         return
     else:
-        await event.edit("Bruh, reply to a fucking file, dumbass")
+        await event.edit("Please reply to a valid file!")
         return
