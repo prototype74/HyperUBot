f5ced926ea4d57fd67d8902d6783c0353e2e95ef
nunopenim <nuno.penim@gmail.com>
2020-09-17 13:29:31 +0100

    general: introduced sideloader

--- userbot/modules/sideloader.py
+++ userbot/modules/sideloader.py
@@ -4,10 +4,15 @@
 # compliance with the PE License
 
 # My stuff
-from userbot import tgclient
+from userbot import tgclient, OS
 
 # Telethon stuff
 from telethon.events import NewMessage
+
+if OS and OS.startswith("win"):
+    USER_MODULES_DIR = ".\\userbot\\modules_user\\"
+else:
+    USER_MODULES_DIR = "./userbot/modules_user/"
 
 @tgclient.on(NewMessage(pattern=r"^\.sideload$", outgoing=True))
 async def sideload(event):
@@ -17,7 +22,8 @@ async def sideload(event):
         if not file.name.endswith(".py"):
             await event.edit("IDIOT, this is not a .py file")
             return
-        await event.edit("Yup, this is a .py file")
+        tgclient.download_media(msg, file_name=USER_MODULES_DIR + file.name)
+        await event.edit("I think the download was finished?")
         return
     else:
         await event.edit("Bruh, reply to a fucking file, dumbass")
