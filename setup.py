c8a242cf68d4cc4d7493fea5d6066ceca505ac51
prototype74 <46874884+prototype74@users.noreply.github.com>
2021-05-29 23:05:51 +0200

    setup: moved main

--- setup.py
+++ setup.py
@@ -151,218 +151,224 @@ def _run_userbot():
     return
 
 
+def main():
+    print("Welcome to HyperUBot's Setup Assistant!")
+    print("This setup will guide you through required processes to run "
+          "HyperUBot on your Machine")
+
+    while True:
+        con = input("Continue? (y/n): ")
+        if con.lower() in ("y", "yes"):
+            break
+        elif con.lower() in ("n", "no"):
+            # not actually a keyboard interruption but same result
+            # as in KeyboardInterrupt exception block
+            raise KeyboardInterrupt
+        else:
+            print(setColorText("Invalid input. Try again...",
+                               Colors.YELLOW))
+
+    if not _userbot_installed():
+        print(setColorText("HyperUBot not installed", Colors.RED_BG))
+        return
+
+    if os.path.exists(os.path.join(".", "userbot", "config.env")) or \
+       os.path.exists(os.path.join(".", "userbot", "config.py")):
+        print(setColorText("Seems like the setup is completed already "
+                           "or done manually", Colors.GREEN))
+        return
+
+    install_telethon_started = False
+
+    while True:
+        try:
+            from telethon import version
+            from telethon.sync import TelegramClient
+            from telethon.sessions import StringSession
+            break
+        except:
+            if not install_telethon_started:
+                print("Telethon package not installed. Installing...")
+                install_telethon_started = True
+                _installTelethon()
+            else:
+                print(setColorText("Unable to import Telethon. "
+                                   "Setup Assistant was not able to "
+                                   "install the Telethon package",
+                                   Colors.RED))
+                return
+
+    telethon_version = tuple(map(int, version.__version__.split(".")))
+    if telethon_version < (1, 21, 1):
+        print("Upgrading Telethon first...\n", Colors.YELLOW)
+        if not _installTelethon(True):
+            return
+
+    print("\nHyperUBot requires, like all other Telegram userbots, "
+          "an API Key, an API Hash and a String Session in order "
+          "to run HyperUBot as an user client.\n"
+          "If not done yet, please go to https://my.telegram.org and\n"
+          "1. log in into your Telegram account\n"
+          "2. create a new application (or use an existing one)\n"
+          "3. get your API Key and Hash (do NOT share these values with "
+          "anyone else!)\n\n"
+          "Please keep the following requirements ready to obtain a "
+          "new String Session:\n"
+          "- Your Telegram application's API Key and Hash (from your "
+          "(existing) application (you created before))\n"
+          "- Your Phone Number which you use for your Telegram account "
+          "(required to log in into your account)\n"
+          "- Your Account's password (Two-Step Verification; "
+          "if enabled)\n")
+
+    while True:
+        inp = input("Ready? (y/n): ")
+        if inp.lower() in ("y", "yes"):
+            break
+        elif inp.lower() in ("n", "no"):
+            print("Alright, cancelling Setup Assistant")
+            raise KeyboardInterrupt
+        else:
+            print(setColorText("Invalid input. Try again...",
+                               Colors.YELLOW))
+
+    api_key, api_hash, string_session = _generateStringSession()
+
+    if not api_key or not api_hash or not string_session:
+        print(
+            setColorText("Setup Assistant failed to get a new "
+                         "string session from your API Key/Hash",
+                         Colors.RED))
+        return
+
+    print("\nAs the API Key, API Hash and String Session are vaild, it's "
+          "important to store them into a configuration file to avoid "
+          "being asked for these values everytime you run HyperUBot. "
+          "HyperUBot supports 2 types: Environment (config.env) and "
+          "Python script (config.py).\n"
+          "Which type of configuration file you wish to have?\n")
+    print("[1] Environment (config.env)\n"
+          "[2] Python script (config.py)\n")
+
+    config_file = None
+
+    while True:
+        inp = input("Your input [1-2] (or 'X' to cancel setup): ")
+        if inp == "1":
+            config_file = os.path.join(".", "userbot", "config.env")
+            break
+        elif inp == "2":
+            config_file = os.path.join(".", "userbot", "config.py")
+            break
+        elif inp.lower() == "x":
+            raise KeyboardInterrupt
+        else:
+            print(setColorText("Invalid input. Try again...",
+                               Colors.YELLOW))
+
+    langs = {"1": {"text": "English/English", "code": "en"},
+             "2": {"text": "German/Deutsch", "code": "de"},
+             "3": {"text": "Portuguese/Português", "code": "pt"}}
+
+    lang_text = ("\nHyperUBot knows some different languages, not much "
+                 "but still it does.\n"
+                 "Please select your preferred language:\n\n")
+
+    first_key_from_langs, last_key_from_langs = (0,)*2
+
+    for key, value in langs.items():
+        if not first_key_from_langs:
+            first_key_from_langs = key
+        language = value.get("text")
+        language_code = value.get("code", "").upper()
+        lang_text += f"[{key}] {language} ({language_code})\n"
+        last_key_from_langs = key
+
+    print(lang_text)
+    lang_code = "en"  # default language
+
+    while True:
+        inp = input("Your input "
+                    f"[{first_key_from_langs}-{last_key_from_langs}] "
+                    "(or 'X' to cancel setup): ")
+        if inp in langs.keys():
+            for key, value in langs.items():
+                if inp == key:
+                    lang_code = value.get("code")
+                    break
+            break
+        elif inp.lower() == "x":
+            raise KeyboardInterrupt
+        else:
+            print(setColorText("Invalid input. Try again...",
+                               Colors.YELLOW))
+
+    try:
+        dl_path = os.path.join(".", "downloads")  # default path
+        if config_file.endswith(".env"):
+            configs = (f'API_KEY = "{api_key}"\n'
+                       f'API_HASH = "{api_hash}"\n'
+                       f'STRING_SESSION = "{string_session}"\n'
+                       f'UBOT_LANG = "{lang_code}"\n'
+                       'LOGGING = False\n'
+                       'LOGGING_CHATID = 0\n'
+                       f'TEMP_DL_DIR = "{dl_path}"\n'
+                       'NOT_LOAD_MODULES = []\n'
+                       'COMMUNITY_REPOS = []\n')
+        else:  # py script
+            configs = ('class ConfigClass(object):\n'
+                       f'{"":4}API_KEY = "{api_key}"\n'
+                       f'{"":4}API_HASH = "{api_hash}"\n'
+                       f'{"":4}STRING_SESSION = "{string_session}"\n'
+                       f'{"":4}UBOT_LANG = "{lang_code}"\n'
+                       f'{"":4}LOGGING = False\n'
+                       f'{"":4}LOGGING_CHATID = 0\n'
+                       f'{"":4}TEMP_DL_DIR = "{dl_path}"\n'
+                       f'{"":4}NOT_LOAD_MODULES = []\n'
+                       f'{"":4}COMMUNITY_REPOS = []\n')
+        with open(config_file, "w") as cfg_file:
+            print(f"Writing configuration file in {config_file}")
+            cfg_file.write(configs)
+        cfg_file.close()
+    except Exception as e:
+        print(
+            setColorText(f"Failed to write configuration file: {e}",
+                         Colors.RED))
+        return
+
+    print("Installing required pip packages...")
+    _install_requirements()
+
+    print(setColorText("\nYaaaay! Setup completed! :P\n", Colors.GREEN))
+    start_bot_text = ("Do you wish to run HyperUBot now? You can always "
+                      "run it by executing 'python3 -m userbot' in "
+                      "HyperUBot's directory later.")
+
+    if IS_WINDOWS:
+        start_bot_text = start_bot_text.replace("python3", "python")
+
+    print(start_bot_text)
+
+    while True:
+        inp = input("Run HyperUBot now? (y/n): ")
+        if inp.lower() in ("y", "yes"):
+            _run_userbot()
+            break
+        elif inp.lower() in ("n", "no"):
+            break
+        else:
+            print(setColorText("Invalid input. Try again...",
+                               Colors.YELLOW))
+    return
+
+
 if __name__ == "__main__":
     try:
-        print("Welcome to HyperUBot's Setup Assistant!")
-        print("This setup will guide you through required processes to run "
-              "HyperUBot on your Machine")
-
-        while True:
-            con = input("Continue? (y/n): ")
-            if con.lower() in ("y", "yes"):
-                break
-            elif con.lower() in ("n", "no"):
-                # not actually a keyboard interruption but same result
-                # as in KeyboardInterrupt exception block
-                raise KeyboardInterrupt
-            else:
-                print(setColorText("Invalid input. Try again...",
-                                   Colors.YELLOW))
-
-        if not _userbot_installed():
-            print(setColorText("HyperUBot not installed", Colors.RED_BG))
-            quit(1)
-
-        if os.path.exists(os.path.join(".", "userbot", "config.env")) or \
-           os.path.exists(os.path.join(".", "userbot", "config.py")):
-            print(setColorText("Seems like the setup is completed already "
-                               "or done manually", Colors.GREEN))
-            quit()
-
-        install_telethon_started = False
-
-        while True:
-            try:
-                from telethon import version
-                from telethon.sync import TelegramClient
-                from telethon.sessions import StringSession
-                break
-            except:
-                if not install_telethon_started:
-                    print("Telethon package not installed. Installing...")
-                    install_telethon_started = True
-                    _installTelethon()
-                else:
-                    print(setColorText("Unable to import Telethon. "
-                                       "Setup Assistant was not able to "
-                                       "install the Telethon package",
-                                       Colors.RED))
-                    quit(1)
-
-        telethon_version = tuple(map(int, version.__version__.split(".")))
-        if telethon_version < (1, 21, 1):
-            print("Upgrading Telethon first...\n", Colors.YELLOW)
-            if not _installTelethon(True):
-                quit(1)
-
-        print("\nHyperUBot requires, like all other Telegram userbots, "
-              "an API Key, an API Hash and a String Session in order "
-              "to run HyperUBot as an user client.\n"
-              "If not done yet, please go to https://my.telegram.org and\n"
-              "1. log in into your Telegram account\n"
-              "2. create a new application (or use an existing one)\n"
-              "3. get your API Key and Hash (do NOT share these values with "
-              "anyone else!)\n\n"
-              "Please keep the following requirements ready to obtain a "
-              "new String Session:\n"
-              "- Your Telegram application's API Key and Hash (from your "
-              "(existing) application (you created before))\n"
-              "- Your Phone Number which you use for your Telegram account "
-              "(required to log in into your account)\n"
-              "- Your Account's password (Two-Step Verification; "
-              "if enabled)\n")
-
-        while True:
-            inp = input("Ready? (y/n): ")
-            if inp.lower() in ("y", "yes"):
-                break
-            elif inp.lower() in ("n", "no"):
-                print("Alright, cancelling Setup Assistant")
-                raise KeyboardInterrupt
-            else:
-                print(setColorText("Invalid input. Try again...",
-                                   Colors.YELLOW))
-
-        api_key, api_hash, string_session = _generateStringSession()
-
-        if not api_key or not api_hash or not string_session:
-            print(
-                setColorText("Setup Assistant failed to get a new "
-                             "string session from your API Key/Hash",
-                             Colors.RED))
-            quit(1)
-
-        print("\nAs the API Key, API Hash and String Session are vaild, it's "
-              "important to store them into a configuration file to avoid "
-              "being asked for these values everytime you run HyperUBot. "
-              "HyperUBot supports 2 types: Environment (config.env) and "
-              "Python script (config.py).\n"
-              "Which type of configuration file you wish to have?\n")
-        print("[1] Environment (config.env)\n"
-              "[2] Python script (config.py)\n")
-
-        config_file = None
-
-        while True:
-            inp = input("Your input [1-2] (or 'X' to cancel setup): ")
-            if inp == "1":
-                config_file = os.path.join(".", "userbot", "config.env")
-                break
-            elif inp == "2":
-                config_file = os.path.join(".", "userbot", "config.py")
-                break
-            elif inp.lower() == "x":
-                raise KeyboardInterrupt
-            else:
-                print(setColorText("Invalid input. Try again...",
-                                   Colors.YELLOW))
-
-        langs = {"1": {"text": "English/English", "code": "en"},
-                 "2": {"text": "German/Deutsch", "code": "de"},
-                 "3": {"text": "Portuguese/Português", "code": "pt"}}
-
-        lang_text = ("\nHyperUBot knows some different languages, not much "
-                     "but still it does.\n"
-                     "Please select your preferred language:\n\n")
-
-        first_key_from_langs, last_key_from_langs = (0,)*2
-
-        for key, value in langs.items():
-            if not first_key_from_langs:
-                first_key_from_langs = key
-            language = value.get("text")
-            language_code = value.get("code", "").upper()
-            lang_text += f"[{key}] {language} ({language_code})\n"
-            last_key_from_langs = key
-
-        print(lang_text)
-        lang_code = "en"  # default language
-
-        while True:
-            inp = input("Your input "
-                        f"[{first_key_from_langs}-{last_key_from_langs}] "
-                        "(or 'X' to cancel setup): ")
-            if inp in langs.keys():
-                for key, value in langs.items():
-                    if inp == key:
-                        lang_code = value.get("code")
-                        break
-                break
-            elif inp.lower() == "x":
-                raise KeyboardInterrupt
-            else:
-                print(setColorText("Invalid input. Try again...",
-                                   Colors.YELLOW))
-
-        try:
-            dl_path = os.path.join(".", "downloads")  # default path
-            if config_file.endswith(".env"):
-                configs = (f'API_KEY = "{api_key}"\n'
-                           f'API_HASH = "{api_hash}"\n'
-                           f'STRING_SESSION = "{string_session}"\n'
-                           f'UBOT_LANG = "{lang_code}"\n'
-                           'LOGGING = False\n'
-                           'LOGGING_CHATID = 0\n'
-                           f'TEMP_DL_DIR = "{dl_path}"\n'
-                           'NOT_LOAD_MODULES = []\n'
-                           'COMMUNITY_REPOS = []\n')
-            else:  # py script
-                configs = ('class ConfigClass(object):\n'
-                           f'{"":4}API_KEY = "{api_key}"\n'
-                           f'{"":4}API_HASH = "{api_hash}"\n'
-                           f'{"":4}STRING_SESSION = "{string_session}"\n'
-                           f'{"":4}UBOT_LANG = "{lang_code}"\n'
-                           f'{"":4}LOGGING = False\n'
-                           f'{"":4}LOGGING_CHATID = 0\n'
-                           f'{"":4}TEMP_DL_DIR = "{dl_path}"\n'
-                           f'{"":4}NOT_LOAD_MODULES = []\n'
-                           f'{"":4}COMMUNITY_REPOS = []\n')
-            with open(config_file, "w") as cfg_file:
-                print(f"Writing configuration file in {config_file}")
-                cfg_file.write(configs)
-            cfg_file.close()
-        except Exception as e:
-            print(
-                setColorText(f"Failed to write configuration file: {e}",
-                             Colors.RED))
-            quit(1)
-
-        print("Installing required pip packages...")
-        _install_requirements()
-
-        print(setColorText("\nYaaaay! Setup completed! :P\n", Colors.GREEN))
-        start_bot_text = ("Do you wish to run HyperUBot now? You can always "
-                          "run it by executing 'python3 -m userbot' in "
-                          "HyperUBot's directory later.")
-
-        if IS_WINDOWS:
-            start_bot_text = start_bot_text.replace("python3", "python")
-
-        print(start_bot_text)
-
-        while True:
-            inp = input("Run HyperUBot now? (y/n): ")
-            if inp.lower() in ("y", "yes"):
-                _run_userbot()
-                break
-            elif inp.lower() in ("n", "no"):
-                break
-            else:
-                print(setColorText("Invalid input. Try again...",
-                                   Colors.YELLOW))
+        main()
     except KeyboardInterrupt:
         print("Exiting...")
-    except Exception as e:
-        print(setColorText(f"Setup Assistant crashed: {e}", Colors.RED_BG))
+    except (BaseException, Exception) as e:
+        print(setColorText(f"Setup Assistant stopped: {e}",
+                           Colors.RED_BG))
         quit(1)
     quit()
