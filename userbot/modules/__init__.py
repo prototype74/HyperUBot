f08b85985723fb9f071a44426cd5bdf074d6d4ab
prototype74 <46874884+prototype74@users.noreply.github.com>
2021-06-26 21:53:39 +0200

    userbot: added __init__ pys
    as suggested from python documentation: https://docs.python.org/3/tutorial/modules.html#packages

---
+++ userbot/modules/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2021 nunopenim @github
+# Copyright 2021 prototype74 @github
+#
+# Licensed under the PEL (Penim Enterprises License), v1.0
+#
+# You may not use this file or any of the content within it, unless in
+# compliance with the PE License
