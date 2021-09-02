b0cdad8febbf9f70e1e1dfdee9efbbbd73c977e5
prototype74 <46874884+prototype74@users.noreply.github.com>
2021-09-02 14:10:01 +0200

    properties: added properties class
    Properties class gives, specially user modules, a new opportunity to store attributes permanently in an own  prop file based on configurations (.ini). This allow the programs to load data from their own prop files e.g. after reboots to avoid any data loss.
    
    Example usage:
    
    from userbot.sysutils.properties import Properties
    
    props = Properties('myprops')
    props.init_props()
    props.setprop('testKey', True)
    
    if props.getprop('testKey'):
        # Do stuff
    else:
        # Do stuff
    
    Multiple programs can't access the same prop file using the Properties class

--- userbot/sysutils/properties.py
+++ userbot/sysutils/properties.py
@@ -6,13 +6,244 @@
 # You may not use this file or any of the content within it, unless in
 # compliance with the PE License
 
-from .sys_funcs import strlist_to_list
+from .sys_funcs import isWindows, strlist_to_list
 from configparser import ConfigParser
 from inspect import currentframe, getouterframes
 from logging import getLogger
 import os
 
 log = getLogger(__name__)
+
+
+class Properties:
+    def __init__(self, name: str):
+        """
+        Initializes the properties from given filename. Name should be a
+        string. Illegal characters in filenames are not allowed.
+
+        Args:
+            name (str): filename
+        """
+        if not isinstance(name, str):
+            raise TypeError("Name should be a string and not "
+                            f"'{type(name).__name__}'")
+        for char in list(name):
+            if char in ("/", "\\", "\r", "\n", "\t", "<", ">", ":", "?",
+                        "*", "\"", "|"):
+                raise ValueError("Name contains illegal characters")
+        if name.lower() == "sysprops":
+            raise ValueError("Name used by core already")
+        self.__parser = ConfigParser()
+        self.__filename = name.replace(" ", "_")
+        self.__path = os.path.join(".", "userbot", "userdata",
+                                   f"{self.__filename}.ini")
+        self.__sections = []
+        self.__prop_present = False
+
+    def __create_props(self, caller, sections=None):
+        """
+        Apparently creates a prop file :)
+        """
+        self.__parser["DEFAULT"] = {"source": caller.replace(os.getcwd(), ".")}
+        if sections and isinstance(sections, (list, tuple)):
+            for section in sections:
+                if section and isinstance(section, str):
+                    section = section.upper()
+                    if not section == "DEFAULT" and \
+                       section not in self.__sections:
+                        self.__parser[section] = {}
+                        self.__sections.append(section)
+        try:
+            with open(self.__path, "w") as propfile:
+                self.__parser.write(propfile)
+            propfile.close()
+            self.__prop_present = True
+        except Exception:
+            log.error("Failed to create props "
+                      f"(requested by {os.path.basename(caller)})",
+                      exc_info=True)
+        return
+
+    def init_props(self, sections=None):
+        """
+        Setup the prop file and checks if the file isn't already used by a
+        different program. There is always a DEFAULT section. New sections
+        are addable by passing a list or tuple to 'sections' arg
+
+        Args:
+            sections (list or tuple): list of section names. Default None
+
+        Example:
+            from userbot.sysutils.properties import Properties
+
+            props = Properties("myprops")
+            props.init_props(["MySection1", "MySection2"])
+        """
+        caller = getouterframes(currentframe(), 2)[1].filename
+        if not os.path.exists(self.__path):
+            self.__create_props(caller, sections)
+            return
+        if os.path.exists(self.__path) and os.path.isfile(self.__path):
+            self.__parser.read(self.__path)
+            source = self.__parser["DEFAULT"].get("source", fallback="")
+            # switch slashes if OS changed
+            if "/" in source and isWindows():
+                source = source.replace("/", "\\")
+            elif "\\" in source and not isWindows():
+                source = source.replace("\\", "/")
+            if source == caller.replace(os.getcwd(), "."):
+                self.__prop_present = True
+            if self.__prop_present:
+                props_modified = False
+                # update prop file if sections differs
+                if sections and isinstance(sections, (list, tuple)):
+                    # load current given sections
+                    for section in sections:
+                        if section and isinstance(section, str):
+                            section = section.upper()
+                            if section not in self.__sections:
+                                self.__sections.append(section)
+                    # compare
+                    temp_sections = self.__parser.sections()
+                    for section in temp_sections:
+                        if section not in self.__sections:
+                            self.__parser.remove_section(section)
+                            if not props_modified:
+                                props_modified = True
+                    temp_sections = self.__parser.sections()  # update list
+                    for section in self.__sections:
+                        if section not in temp_sections:
+                            self.__parser[section] = {}
+                            if not props_modified:
+                                props_modified = True
+                else:
+                    # if a list of sections was given in past but now removed,
+                    # all sections will be removed from file except DEFAULT too
+                    temp_sections = self.__parser.sections()
+                    for section in temp_sections:
+                        if not section == "DEFAULT":
+                            self.__parser.remove_section(section)
+                            if not props_modified:
+                                props_modified = True
+                if props_modified:
+                    try:
+                        with open(self.__path, "w") as propfile:
+                            self.__parser.write(propfile)
+                        propfile.close()
+                    except Exception:
+                        log.error("[INIT] Failed to update props "
+                                  f"(requested by {os.path.basename(caller)})",
+                                  exc_info=True)
+                        self.__prop_present = False
+            else:
+                log.error(f"[INIT] Prop '{self.__path}' exists but used "
+                          "by a different program already "
+                          f"(requested by {os.path.basename(caller)})")
+        elif os.path.exists(self.__path) and os.path.isdir(self.__path):
+            log.error(f"[INIT] '{self.__path}' exists as directory, not as "
+                      f"file (requested by {os.path.basename(caller)})")
+        else:
+            log.error(f"[INIT] Prop '{self.__path}' not found "
+                      f"(requested by {os.path.basename(caller)})")
+        return
+
+    def setprop(self, key: str, value, section=None) -> bool:
+        """
+        Add or update an (existing) key with a new value. Key will always be
+        stored to DEFAULT section if no 'section' is given or doesn't exist.
+
+        Args:
+            key (str): existing/new key
+            value: new value
+            section (str): name of section. Default None
+
+        Example:
+            from userbot.sysutils.properties import Properties
+
+            props = Properties("myprops")
+            props.init_props(["MySection1", "MySection2"])
+            props.setprop("testKey1", "testValue1")  # DEFAULT
+            props.setprop("testKey2", True, "MySection1")
+
+        Returns:
+            True if action was successful else False
+        """
+        caller = getouterframes(currentframe(), 2)[1].filename
+        if not self.__prop_present:
+            log.warning(f"[SETPROP] Prop '{self.__path}' not found "
+                        f"(requested by {os.path.basename(caller)})")
+            return False
+        self.__parser.read(self.__path)
+        target_section = "DEFAULT"
+        prop_set = False
+        if section and isinstance(section, str):
+            section = section.upper()
+            if section in self.__sections:
+                target_section = section
+        if key.lower() == "source" and target_section == "DEFAULT":
+            return False
+        value = str(value)
+        if value.lower() == "false":
+            value = "no"
+        elif value.lower() == "true":
+            value = "yes"
+        try:
+            self.__parser[target_section][key.lower()] = value
+            with open(self.__path, "w") as propfile:
+                self.__parser.write(propfile)
+            propfile.close()
+            prop_set = True
+        except Exception:
+            log.error(f"[SETPROP] Failed to update prop '{self.__path}' "
+                      f"(requested by {os.path.basename(caller)})",
+                      exc_info=True)
+        return True if prop_set else False
+
+    def getprop(self, key: str, section=None) -> bool:
+        """
+        Load the value from given key. getprop will always check in DEFAULT
+        section if no 'section' is given or doesn't exist.
+
+        Args:
+            key (str): existing key
+            section (str): name of section. Default None
+
+        Example:
+            from userbot.sysutils.properties import Properties
+
+            props = Properties("myprops")
+            props.init_props(["MySection1", "MySection2"])
+            props.setprop("testKey1", "testValue1")  # DEFAULT
+            props.setprop("testKey2", True, "MySection1")
+
+            if props.getprop("testKey2", "MySection1"):
+                # do stuff
+
+        Returns:
+            the value from key else None if key doesn't exist
+        """
+        caller = getouterframes(currentframe(), 2)[1].filename
+        if not self.__prop_present:
+            log.warning(f"[GETPROP] Prop '{self.__path}' not found "
+                        f"(requested by {os.path.basename(caller)})")
+            return None
+        self.__parser.read(self.__path)
+        target_section = "DEFAULT"
+        if section and isinstance(section, str):
+            section = section.upper()
+            if section in self.__sections:
+                target_section = section.upper()
+        value = self.__parser[target_section].get(key.lower(), fallback=None)
+        if value:
+            if value.startswith("[") and value.endswith("]"):
+                value = strlist_to_list(value)
+            elif value.lower() == "no":
+                value = False
+            elif value.lower() == "yes":
+                value = True
+            elif value.isnumeric():
+                value = int(value)
+        return value
 
 
 class _SysProperties:
