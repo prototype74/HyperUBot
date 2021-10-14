121c23a8424888af44762543948b61a8a8331c9c
prototype74 <46874884+prototype74@users.noreply.github.com>
2021-10-14 20:49:15 +0200

    sysutils: added getpass
    similar to Python's getpass module but the typed password will be echoed as asterisk
    
    Example:
    Your password: *******************

---
+++ userbot/sysutils/getpass.py
@@ -0,0 +1,54 @@
+# Copyright 2021 nunopenim @github
+# Copyright 2021 prototype74 @github
+#
+# Licensed under the PEL (Penim Enterprises License), v1.0
+#
+# You may not use this file or any of the content within it, unless in
+# compliance with the PE License
+
+from .sys_funcs import isWindows
+import sys
+
+if isWindows():
+    import msvcrt
+else:
+    import termios
+    import tty
+
+
+def _GetwchPOSIX():
+    if isWindows():
+        return ""
+    # based on: https://docs.python.org/3/library/termios.html#example
+    fd = sys.stdin.fileno()
+    prev_attr = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd, termios.TCSAFLUSH)
+        char = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, prev_attr)
+    return char
+
+
+_getwch = msvcrt.getwch if isWindows() else _GetwchPOSIX
+
+
+def getpass(prompt):
+    print(prompt, end="", flush=True)
+    password_chars = []
+    while True:
+        char = _getwch()
+        if ord(char) in (10, 13):
+            break
+        elif ord(char) in (8, 127):  # Windows: 8, POSIX: 127
+            if len(password_chars) > 0:
+                password_chars.pop()
+                print("\b \b", end="", flush=True)
+        elif ord(char) in (3,):
+            raise KeyboardInterrupt
+        else:
+            password_chars.append(char)
+            print("*", end="", flush=True)
+    print()
+    password = "".join(password_chars)
+    return password
