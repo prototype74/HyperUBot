ae61eb84868b9c6d66361ae08141ada30570a124
prototype74 <46874884+prototype74@users.noreply.github.com>
2022-02-19 21:19:02 +0100

    _core: added Access Controller
    To protect 'private' attributes (globals, functions, classes) which are still callable from modules (also built-in), the Access Controller has been added to block these calls, as the private attributes (mainly those starting with underscores) are meant for the core services only. This feature may increase the security of some sensitive data too, well it's still Python tho.
    
    The function _protectedAccess() is based on https://stackoverflow.com/a/922693

---
+++ userbot/_core/access_controller.py
@@ -0,0 +1,66 @@
+# Copyright 2022 nunopenim @github
+# Copyright 2022 prototype74 @github
+#
+# Licensed under the PEL (Penim Enterprises License), v1.0
+#
+# You may not use this file or any of the content within it, unless in
+# compliance with the PE License
+
+from userbot.sysutils.errors import UnauthorizedAccessError
+from inspect import currentframe, getouterframes
+from logging import getLogger, Logger
+from os.path import basename, dirname, join
+import sys
+
+
+# based on: https://stackoverflow.com/a/922693
+def _protectedAccess(module, attrs: list,
+                     allowed: tuple = (), warn_msg: str = "", mlogger=None):
+    if isinstance(mlogger, Logger):
+        logger = mlogger
+    else:
+        logger = getLogger(__name__)
+
+    all_attrs = ["_protectedAccess"] + attrs
+
+    class FilterAccess():
+        def __getattribute__(self, attr):
+            if attr in all_attrs:
+                caller = getouterframes(currentframe(), 2)[1]
+                caller_name = caller.filename
+                caller_line = caller.lineno
+                if dirname(caller_name).endswith(
+                    (join("userbot", "modules"),
+                     join("userbot", "modules_user"))):
+                    if not caller_name.endswith(allowed):
+                        if warn_msg:
+                            logger.warning(
+                                warn_msg.format(
+                                    attr, basename(caller_name), caller_line))
+                        raise UnauthorizedAccessError()
+            return getattr(module, attr)
+
+        def __setattr__(self, attr, value):
+            if attr in all_attrs:
+                caller = getouterframes(currentframe(), 2)[1]
+                caller_name = caller.filename
+                caller_line = caller.lineno
+                if dirname(caller_name).endswith(
+                    (join("userbot", "modules"),
+                     join("userbot", "modules_user"))):
+                    if not caller_name.endswith(allowed):
+                        if warn_msg:
+                            logger.warning(
+                                warn_msg.format(
+                                    attr, basename(caller_name), caller_line))
+                        raise UnauthorizedAccessError()
+            return setattr(module, attr, value)
+    return FilterAccess()
+
+
+sys.modules[__name__] = _protectedAccess(
+    sys.modules[__name__],
+    attrs=[],
+    warn_msg=("Modules are not allowed to use the "
+              "Access Controller (requested by {1}:{2})")
+)
